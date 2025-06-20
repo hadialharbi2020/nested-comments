b327b2024c9454e45dd7125eb8bdd5d2eca6f750
HadiAlharbi <hadionline1994@gmail.com>
2025-06-20 20:17:32 +0300

    Prepare v1.0.1: RTL, Arabic, edit/delete, tests 4

--- resources/views/livewire/reaction-panel.blade.php
+++ resources/views/livewire/reaction-panel.blade.php
@@ -1,17 +1,11 @@
 <div wire:poll.10s class="flex flex-wrap items-center justify-end gap-2 text-right" dir="rtl">
-    @foreach($this->record->reactions_map->filter(fn($reaction) => collect($reaction)->get('reactions') > 0) as
-    $reaction => $attribs)
-    <x-filament::button x-on:click="$wire.react('{{$reaction}}')" @class([ 'font-light'=> true,
-        ])
-        title="{{$reaction}} {{collect($attribs)->get('reactions')}}
-        {{str(collect($attribs)->get('name'))->plural(collect($attribs)->get('reactions'))}}"
-        tooltip="{{$reaction}} {{collect($attribs)->get('reactions')}}
-        {{str(collect($attribs)->get('name'))->plural(collect($attribs)->get('reactions'))}}"
-        :outlined="true"
-        :color="collect($attribs)->get('meToo') ? 'primary' : 'gray'" size="xs"
-        >
-        <span class="text-md">{{$reaction}} {{\Illuminate\Support\Number::forHumans(collect($attribs)->get('reactions'),
-            maxPrecision: 2)}}</span>
+    @foreach($this->record->reactions_map->filter(fn($reaction) => collect($reaction)->get('reactions') > 0) as $reaction => $attribs)
+    <x-filament::button x-on:click="$wire.react('{{ $reaction }}')" class="font-light"
+        title="{{ $reaction }} {{ collect($attribs)->get('reactions') }} {{ str(collect($attribs)->get('name'))->plural(collect($attribs)->get('reactions')) }}"
+        tooltip="{{ $reaction }} {{ collect($attribs)->get('reactions') }} {{ str(collect($attribs)->get('name'))->plural(collect($attribs)->get('reactions')) }}"
+        :outlined="true" :color="collect($attribs)->get('meToo') ? 'primary' : 'gray'" size="xs">
+        <span class="text-md">{{ $reaction }} {{ \Illuminate\Support\Number::forHumans(collect($attribs)->get('reactions'),
+            maxPrecision: 2) }}</span>
     </x-filament::button>
     @endforeach
     <x-filament::dropdown placement="bottom-start">
